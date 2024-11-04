7ddad66db07b435cb0339e5c091e784a4793295d
Anthony Fontaine <anthony.fontaine454@gmail.com>
2024-11-04 14:21:28 +0100

    Got the card gallery with the wrong api for the moment
    And got style for the card

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,53 +1,23 @@
 import styles from '../../styles/gallery.module.scss';
 import Image from 'next/image';
+import useModularFetch from '../../hooks/modularFetch.js';
 
 
 export default function Gallery() {
+    const { entities: galleryData, loading, error } = useModularFetch('/api/gallery3d');
+
   return (
       <div className={styles.galleryContainer}>
         <div className={styles.galleryWrap}>
-            <div className={styles.galleryCard}>
-            <Image src={'/#'} width={100} height={100} alt={'#3d viewer'} />
-            <div className={styles.galleryCardContent}>
-                    <p className={styles.cardTitle}>Title</p>
-                    <p className={styles.cardInfo}>Information</p>
-                </div>
-            </div>
-            <div className={styles.galleryCard}>
-            <Image src={'/#'} width={100} height={100} alt={'#3d viewer'} />
-            <div className={styles.cardContent}>
-                    <p className={styles.cardTitle}>Title</p>
-                    <p className={styles.cardInfo}>Information</p>
-                </div>
-            </div>
-            <div className={styles.galleryCard}>
-            <Image src={'/#'} width={100} height={100} alt={'#3d viewer'} />
-            <div className={styles.cardContent}>
-                    <p className={styles.cardTitle}>Title</p>
-                    <p className={styles.cardInfo}>Information</p>
-                </div>
-            </div>
-            <div className={styles.galleryCard}>
-            <Image src={'/#'} width={100} height={100} alt={'#3d viewer'} />
-            <div className={styles.cardContent}>
-                    <p className={styles.cardTitle}>Title</p>
-                    <p className={styles.cardInfo}>Information</p>
-                </div>
-            </div>
-            <div className={styles.galleryCard}>
-            <Image src={'/#'} width={100} height={100} alt={'#3d viewer'} />
-            <div className={styles.cardContent}>
-                    <p className={styles.cardTitle}>Title</p>
-                    <p className={styles.cardInfo}>Information</p>
-                </div>
-            </div>
-            <div className={styles.galleryCard}>
-            <Image src={'/#'} width={100} height={100} alt={'#3d viewer'} />
-            <div className={styles.cardContent}>
-                    <p className={styles.cardTitle}>Title</p>
-                    <p className={styles.cardInfo}>Information</p>
-                </div>
-            </div>
+        {galleryData.map((item) => (
+                 <div key={item._id} className={styles.galleryCard}>
+                 <Image src={item.imageurl} width={300} height={200} alt={'#3d viewer'} />
+                 <div className={styles.galleryCardContent}>
+                         <p className={styles.cardTitle}>{item.title}</p>
+                         <p className={styles.cardInfo}>{item.description}</p>
+                     </div>
+                 </div>
+        ))}
         </div>
         </div>
   );
